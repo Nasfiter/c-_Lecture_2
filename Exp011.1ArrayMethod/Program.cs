46387b820233f5b9ada42b07de22e4cbc2d98afa
Alex Nejen <alex_nas@Mac-mini-Alex.local>
2022-05-28 19:03:52 -0400

    Exp 011

---
+++ Exp011.1ArrayMethod/Program.cs
@@ -0,0 +1,39 @@
+﻿
+void FIllArray(int[] collection)
+{
+    int index = 0;
+    int Length = collection.Length;
+    while (index < Length )
+    {
+        collection[index] = new Random().Next(1, 100);
+
+
+    index++;
+    }
+
+
+
+    
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int possition = 0;
+    while(possition < count)
+    {
+        Console.WriteLine(col[possition]);
+
+        possition++;
+    }
+
+}
+
+
+
+
+
+int[] Array = new int[8];
+
+FIllArray(Array);
+PrintArray(Array);
